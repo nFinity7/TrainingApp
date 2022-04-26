6f81b659f61f85a9a7c343da736bd7660f9a6db4
sebastianjuras <sebastian.juras@gmail.com>
2022-04-26 22:59:02 +0200

    Timer functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,48 @@
-import React from "react"
+import React, { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import "./Timer.css";
+
+const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">Finish the exercise</div>;
+    }
+  
+    return (
+      <div className="timer">
+        <div className="text">Remaining</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">seconds</div>
+      </div>
+    );
+  };
 
 export default function Timer() {
+    const [key, setKey] = useState(0);
     return (
-    <div>Timer</div>
+        <div className="Timer">
+        <h1>
+          Training Timer
+        </h1>
+        <div className="timer-wrapper">
+          <CountdownCircleTimer
+            key={key}
+            isPlaying
+            duration={30}
+            size={250}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[20, 15, 0]}
+            onComplete={() => console.log('123')}
+  
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </div>
+        <div className="button-wrapper">
+          <button onClick={() => setKey(prevKey => prevKey + 1)}>
+            Restart Timer
+          </button>
+        </div>
+      </div>
     )
 }
